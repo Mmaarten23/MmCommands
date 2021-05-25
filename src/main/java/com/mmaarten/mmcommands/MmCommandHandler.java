67530ca6d2f8ce729d2d036070f2cbf7afa4c520
TheSummerGrinch <66258844+TheSummerGrinch@users.noreply.github.com>
2021-05-25 22:23:24 +0200

    Updated imports.

--- src/main/java/com/mmaarten/mmcommands/MmCommandHandler.java
+++ src/main/java/com/mmaarten/mmcommands/MmCommandHandler.java
@@ -29,8 +29,9 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 
